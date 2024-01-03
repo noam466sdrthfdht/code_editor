c7e83da6040a1c0d6d10b30181e4ae874bee2a25
noam466sdrthfdht <83455940+noam466sdrthfdht@users.noreply.github.com>
2024-01-03 17:55:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,31 @@
 import streamlit as st
-import openai
+from openai import OpenAI
 
 # Set your OpenAI API key
-openai.api_key = "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"
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key="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"
+)
 # Function for AI text generation using GPT-2
 def ai_code_completion(user_code):
     try:
         # Make a request to the OpenAI API for text generation using GPT-2
-        response = openai.Completion.create(
-            engine="davinci-codex",
-            prompt=user_code,
-            max_tokens=100  # Adjust as needed
-        )
+
+
+        response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_code,
+            }
+        ],
+        model="davinci-codex",
+    )
+
+
+
+    
         completed_code = response.choices[0].text.strip()
     except Exception as e:
         st.error(f"Error during AI code completion: {e}")
